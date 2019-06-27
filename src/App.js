6d0f66ffdca4ae5abf60bec56402856c7ba7c5e8
amrad21 <arridhaamrad@gmail.com>
2019-06-27 21:01:38 +0700

    Navbar Complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+            <Switch>
+              <div className="container">
+                <Route exact path="/about" component={About} />
+              </div>
+            </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react';
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <nav className="navbar bg-primary">
+        <h1><i class="fab fa-github"></i> Github Finder</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+    </Fragment>
+  )
+}
+
+export default Navbar
