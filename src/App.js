b12560a3d6ac4b56eff0f7ee009c0c017709c2f1
amrad21 <arridhaamrad@gmail.com>
2019-06-30 22:45:41 +0700

    Add Home and NotFound

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import User from './components/users/User';
 import './App.css';
 
@@ -59,15 +59,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route exact path='/' render={props => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )}
-                />
+                <Route exact path='/' component={Home}/>
                 <Route exact path="/about" component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound}/>
               </Switch>
             </div>
           </div>
